49f805913fcb5ada4e24b5a398389641ee21de1d
Pavan Vora <pavanvora.cs@gmail.com>
2018-10-10 16:35:34 +0530

    middleware and routes added

--- app.js
+++ app.js
@@ -4,12 +4,26 @@ var cors = require('cors');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
+const authMiddleware = require('./middlewares/authMiddleware');
+
+const authController = require('./controllers/authController');
+const accountController = require('./controllers/accountController');
+
 var app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
+app.use('/api/auth', authController);
+
+// verify token and otp
+app.use(authMiddleware.verifyToken);
+app.use(authMiddleware.verifyOTP);
+
+// Authorized API
+app.use('/api/account', accountController);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
